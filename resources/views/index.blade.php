2075879083694ca011cfe2856a659b962b3dcc9c
ETY001 <ety001@domyself.me>
2017-01-18 22:36:57 +0700

    ignore

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <example></example>
+        </div>
+        <script src="{{ elixir('js/app.js') }}"></script>
+    </body>
+</html>
